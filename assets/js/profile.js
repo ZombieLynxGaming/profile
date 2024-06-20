7ada9cc81b2b5607a5c8af8a8136ad6cc9fb89b1
ZombieLynxGaming <126105917+ZombieLynxGaming@users.noreply.github.com>
2024-06-19 21:50:50 -0500

    test 11

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,23 +1,20 @@
-// profile.js
-
-const API_BASE_URL = "https://profile.zlg.gg:1111";  // Update to HTTPS
-
-function fetchUserProfile() {
-    console.log('Fetching user profile');
+document.addEventListener('DOMContentLoaded', function() {
     const token = new URLSearchParams(window.location.search).get('token');
-    console.log('Token:', token);  // Log the token to verify it's being extracted correctly
     if (!token) {
         console.error('No token found');
         return;
     }
 
-    fetch(`${API_BASE_URL}/api/profile`, {
+    fetchUserProfile(token);
+});
+
+function fetchUserProfile(token) {
+    fetch('https://profile.zlg.gg:1111/api/profile', {  // Update to HTTPS
         headers: {
             'Authorization': `Bearer ${token}`
         }
     })
     .then(response => {
-        console.log('Response received:', response); // Log the response
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
@@ -34,33 +31,42 @@ function fetchUserProfile() {
 }
 
 function displayUserProfile(data) {
-    console.log('Displaying user profile:', data);  // Log user data for verification
-    const usernameElements = document.querySelectorAll('.username');
-    const avatarElements = document.querySelectorAll('.avatar');
-    const tribeElements = document.querySelectorAll('.tribe');
-    const pointsElements = document.querySelectorAll('.points');
-    const membershipElements = document.querySelectorAll('.membership');
-    const killsElements = document.querySelectorAll('.kills');
-    const deathsElements = document.querySelectorAll('.deaths');
-    const kdElements = document.querySelectorAll('.kd');
-    const dailiesElements = document.querySelectorAll('.dailies');
-    const weekliesElements = document.querySelectorAll('.weeklies');
-    const bkilledElements = document.querySelectorAll('.bkilled');
+    const membershipColors = {
+        'Admins': 'radial-gradient(circle, #dd163b, #8b0000)',
+        'Vibranium': 'radial-gradient(circle, #d8ba0e, #d8ba0e)',
+        'Diamond': 'radial-gradient(circle, #d8ba0e, #d8ba0e)',
+        'Gold': 'radial-gradient(circle, #d8ba0e, #d8ba0e)',
+        'Standard': 'radial-gradient(circle, #139257, #139257)'
+    };
 
-    usernameElements.forEach(elem => elem.textContent = data.displayName);
-    avatarElements.forEach(elem => elem.src = data.avatar);
-    tribeElements.forEach(elem => elem.textContent = data.tribe);
-    pointsElements.forEach(elem => elem.textContent = data.points);
-    membershipElements.forEach(elem => {
-        elem.textContent = data.membership;
-        setMembershipColor(elem); // Set the color based on the membership
-    });
-    killsElements.forEach(elem => elem.textContent = data.kills);
-    deathsElements.forEach(elem => elem.textContent = data.deaths);
-    kdElements.forEach(elem => elem.textContent = data.kd);
-    dailiesElements.forEach(elem => elem.textContent = data.dailies);
-    weekliesElements.forEach(elem => elem.textContent = data.weeklies);
-    bkilledElements.forEach(elem => elem.textContent = data.bkilled);
+    const usernameElement = document.querySelectorAll('.username');
+    const avatarElement = document.querySelectorAll('.avatar');
+    const tribeElement = document.querySelectorAll('.tribe');
+    const pointsElement = document.querySelectorAll('.points');
+    const membershipElement = document.querySelectorAll('.membership');
+    const killsElement = document.querySelectorAll('.kills');
+    const deathsElement = document.querySelectorAll('.deaths');
+    const kdElement = document.querySelectorAll('.kd');
+    const dailiesElement = document.querySelectorAll('.dailies');
+    const weekliesElement = document.querySelectorAll('.weeklies');
+    const bkilledElement = document.querySelectorAll('.bkilled');
+
+    if (usernameElement) usernameElement.forEach((elem) => elem.textContent = data.displayName);
+    if (avatarElement) avatarElement.forEach((elem) => elem.src = data.avatar);
+    if (tribeElement) tribeElement.forEach((elem) => elem.textContent = data.tribe);
+    if (pointsElement) pointsElement.forEach((elem) => elem.textContent = data.points);
+    if (membershipElement) {
+        membershipElement.forEach((elem) => {
+            elem.textContent = data.membership;
+            elem.style.backgroundColor = membershipColors[data.membership] || membershipColors['Standard'];
+        });
+    }
+    if (killsElement) killsElement.forEach((elem) => elem.textContent = data.kills);
+    if (deathsElement) deathsElement.forEach((elem) => elem.textContent = data.deaths);
+    if (kdElement) kdElement.forEach((elem) => elem.textContent = data.kd);
+    if (dailiesElement) dailiesElement.forEach((elem) => elem.textContent = data.dailies);
+    if (weekliesElement) weekliesElement.forEach((elem) => elem.textContent = data.weeklies);
+    if (bkilledElement) bkilledElement.forEach((elem) => elem.textContent = data.bkilled);
 
     // For leaderboard
     const leaderboardElement = document.getElementById('leaderboard');
@@ -73,28 +79,6 @@ function displayUserProfile(data) {
                 <td class="text-center">${player.KD ? player.KD.toFixed(2) : 'N/A'}</td>
             </tr>
         `).join('');
-    }
-}
-
-function setMembershipColor(elem) {
-    const membership = elem.textContent;
-    switch (membership) {
-        case 'Admins':
-            elem.style.background = 'radial-gradient(circle, #dd163b, #8b0000)'; // Red gradient
-            break;
-        case 'Vibranium':
-            elem.style.background = 'radial-gradient(circle, #8a2be2, #4b0082)'; // BlueViolet gradient
-            break;
-        case 'Diamond':
-            elem.style.background = 'radial-gradient(circle, #00bfff, #1e90ff)'; // LightBlue gradient
-            break;
-        case 'Gold':
-            elem.style.background = 'radial-gradient(circle, #ffd700, #daa520)'; // Gold gradient
-            break;
-        case 'Standard':
-        default:
-            elem.style.background = 'radial-gradient(circle, #6c757d, #495057)'; // Silver gradient
-            break;
     }
 }
 
--- assets/js/user.js
+++ assets/js/user.js
@@ -1,86 +1,84 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const token = new URLSearchParams(window.location.search).get('token');
-  if (!token) {
-      console.error('No token found');
-      return;
-  }
+    const token = new URLSearchParams(window.location.search).get('token');
+    if (!token) {
+        console.error('No token found');
+        return;
+    }
 
-  fetchUserProfile(token);
+    fetchUserProfile(token);
 });
 
 function fetchUserProfile(token) {
-  fetch('https://profile.zlg.gg:1111/api/profile', {  // Update to HTTPS
-      headers: {
-          'Authorization': `Bearer ${token}`
-      }
-  })
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.json();
-  })
-  .then(data => {
-      console.log('User data:', data);
-      displayUserProfile(data);
-      initializeCharts(data); // Initialize charts with user data
-  })
-  .catch(error => {
-      console.error('Error fetching user profile:', error);
-  });
+    fetch('https://profile.zlg.gg:1111/api/profile', {  // Update to HTTPS
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('User data:', data);
+        displayUserProfile(data);
+        initializeCharts(data); // Initialize charts with user data
+    })
+    .catch(error => {
+        console.error('Error fetching user profile:', error);
+    });
 }
 
 function displayUserProfile(user) {
-  document
-      .querySelectorAll(".username")
-      .forEach((elem) => (elem.innerText = user.displayName));
-  document
-      .querySelectorAll(".avatar")
-      .forEach((elem) => (elem.src = user.avatar));
-  document
-      .querySelectorAll(".points")
-      .forEach((elem) => (elem.innerText = user.points));
-  document.querySelectorAll(".membership").forEach((elem) => {
-      elem.innerText = user.membership;
-  });
-  document
-      .querySelectorAll(".message")
-      .forEach((elem) => (elem.innerText = user.message));
-  document
-      .querySelectorAll(".tribe")
-      .forEach((elem) => (elem.innerText = user.tribe));
-  document
-      .querySelectorAll(".kills")
-      .forEach((elem) => (elem.innerText = user.kills));
-  document
-      .querySelectorAll(".deaths")
-      .forEach((elem) => (elem.innerText = user.deaths));
-  document
-      .querySelectorAll(".kd")
-      .forEach((elem) => (elem.innerText = user.kd));
-  document
-      .querySelectorAll(".dailies")
-      .forEach((elem) => (elem.innerText = user.dailies));
-  document
-      .querySelectorAll(".weeklies")
-      .forEach((elem) => (elem.innerText = user.weeklies));
-  document
-      .querySelectorAll(".bkilled")
-      .forEach((elem) => (elem.innerText = user.bkilled));
+    document
+        .querySelectorAll(".username")
+        .forEach((elem) => (elem.innerText = user.displayName));
+    document
+        .querySelectorAll(".avatar")
+        .forEach((elem) => (elem.src = user.avatar));
+    document
+        .querySelectorAll(".points")
+        .forEach((elem) => (elem.innerText = user.points));
+    document.querySelectorAll(".membership").forEach((elem) => {
+        elem.innerText = user.membership;
+    });
+    document
+        .querySelectorAll(".message")
+        .forEach((elem) => (elem.innerText = user.message));
+    document
+        .querySelectorAll(".tribe")
+        .forEach((elem) => (elem.innerText = user.tribe));
+    document
+        .querySelectorAll(".kills")
+        .forEach((elem) => (elem.innerText = user.kills));
+    document
+        .querySelectorAll(".deaths")
+        .forEach((elem) => (elem.innerText = user.deaths));
+    document
+        .querySelectorAll(".kd")
+        .forEach((elem) => (elem.innerText = user.kd));
+    document
+        .querySelectorAll(".dailies")
+        .forEach((elem) => (elem.innerText = user.dailies));
+    document
+        .querySelectorAll(".weeklies")
+        .forEach((elem) => (elem.innerText = user.weeklies));
+    document
+        .querySelectorAll(".bkilled")
+        .forEach((elem) => (elem.innerText = user.bkilled));
 
-  const leaderboard = document.getElementById("leaderboard");
-  leaderboard.innerHTML = "";
+    const leaderboard = document.getElementById("leaderboard");
+    leaderboard.innerHTML = "";
 
-  user.leaderboard.forEach((player, index) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-          <td class="text-center">${index + 1}</td>
-          <td>${player.Name}</td>
-          <td class="text-center">${player.PlayerKills}</td>
-          <td class="text-center">${
-              player.KD ? player.KD.toFixed(2) : "N/A"
-          }</td>
-      `;
-      leaderboard.appendChild(row);
-  });
+    user.leaderboard.forEach((player, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td class="text-center">${index + 1}</td>
+            <td>${player.Name}</td>
+            <td class="text-center">${player.PlayerKills}</td>
+            <td class="text-center">${player.KD ? player.KD.toFixed(2) : "N/A"}</td>
+        `;
+        leaderboard.appendChild(row);
+    });
 }
