0dd33134fb6316d2d9b905b3c4d2108cd5177771
ZombieLynxGaming <126105917+ZombieLynxGaming@users.noreply.github.com>
2024-07-01 09:39:07 -0500

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,10 +94,10 @@ function displayUserProfile(user) {
         memberDiv.setAttribute('data-bs-content', `
             <div class="text-center">
                 <div class="profile-card d-flex align-items-center row">
-                    <div class="profile-image col-6 ps-2 rounded d-flex justify-content-center" style="border: 1px solid green;">
+                    <div class="profile-image col-6 ps-0 rounded d-flex justify-content-center" style="border: 1px solid green;">
                         <img class="text-center align-self-center rounded-circle popover-avatar" src="${member.avatar || ''}" alt="Profile Image">
                     </div>
-                    <div class="profile-info flex-grow-1 col-6">
+                    <div class="profile-info flex-grow-1 col-6 ps-0">
                         <div class=" my-0 text-center text-white tribe-member-text-1 m-0 p-0"><strong>${member.name}</strong></div>
                         <div class=" fw-light my-0 text-center text-white tribe-member-text-2 m-0 p-0">${user.tribe}</div>
                         <div class="d-flex justify-content-around mt-3">
