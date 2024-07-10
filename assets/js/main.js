d601af3d76b64c21805ca42eb3ff6e25c103c875
ZombieLynxGaming <126105917+ZombieLynxGaming@users.noreply.github.com>
2024-07-09 23:20:18 -0500

    preload lynx gif

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,19 @@ import { displayMedals } from './medals.js';
 import { fetchMockUserProfile, fetchMockUserStats } from './mock.js';
 import { initializeCharts } from './charts.js';
 
+/* -------------------------------------------------------------------------- */
+/*                                  Preloader                                 */
+/* -------------------------------------------------------------------------- */
+var preloaderInit = function preloaderInit() {
+  var preloader = document.querySelector('.preloader');
+  setTimeout(function () {
+    preloader?.classList.add('loaded');
+    setTimeout(function () {
+      preloader?.remove();
+    }, 0); // Remove preloader immediately after setting it to 'loaded'
+  }, 3000); // Keep the preloader for 5 seconds
+};
+
 const SESSION_DURATION = 14.5 * 60 * 1000; // 14.5 minutes in milliseconds
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,6 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.error("No token found, using mock data");
     fetchMockUserProfile();
     fetchMockUserStats();
+    preloaderInit(); // Initialize preloader for mock data
     return;
   }
 
@@ -27,11 +41,13 @@ document.addEventListener("DOMContentLoaded", () => {
     displayMedals(user);
     initializePopovers();
     initializeCharts(user);
+    preloaderInit(); // Initialize preloader for real data
   }).catch(error => {
     console.error("Error fetching user profile:", error);
     fetchMockUserProfile();
+    preloaderInit(); // Initialize preloader for mock data on error
   });
 });
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
